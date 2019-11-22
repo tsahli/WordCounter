f758b11d94c5fb828c4d2e69c76bf6640c7f6c7a
Taylor Sahli <taylorsahli837@gmail.com>
2019-11-22 11:49:09 -0700

    common words are filtered out of results

--- WordCounter/WordCounter.py
+++ WordCounter/WordCounter.py
@@ -6,9 +6,9 @@ import operator
 
 while True:
     try:
-        textFile = raw_input('Enter name of txt file: ')
+        textFile = input('Enter name of txt file: ')
         textFile = textFile + '.txt'
-        file = open(textFile, 'r')
+        file = open(textFile, 'r', encoding='utf8')
         break
     except:
         print('File not found!')
@@ -16,12 +16,21 @@ while True:
         
 while True:
     try:
-        wordMinimum = int(raw_input('Show me words that occur more than ___ times: '))
+        wordMinimum = int(input('Show me words that occur more than ___ times: '))
         break
     except:
         print('Please enter a number.')
         continue
-        
+
+wordsToFilter = ['THE', 'BE', 'AND', 'OF', 'A', 'IN', 'TO', 'HAVE', 'IT', 'I', 'THAT', 'FOR', 'YOU', 'HE', 'WITH', 'ON',
+                'DO', 'SAY', 'THIS', 'THEY', 'IS', 'AN', 'AT', 'BUT', 'WE', 'HIS', 'FROM', 'THAT', 'NOT', 'BY', 'SHE',
+                'OR', 'AS', 'WHAT', 'GO', 'THEIR', 'CAN', 'WHO', 'GET', 'IF', 'WOULD', 'HER', 'ALL', 'MY', 'MAKE', 'ABOUT',
+                'KNOW', 'WILL', 'AS', 'UP', 'ONE', 'TIME', 'HAS', 'BEEN', 'THERE', 'YEAR', 'SO', 'THINK', 'WHEN', 'WHICH',
+                'THEM', 'SOME', 'ME', 'PEOPLE', 'TAKE', 'OUT', 'INTO', 'JUST', 'SEE', 'HIM', 'YOUR', 'COME', 'COULD', 'NOW',
+                'THAN', 'LIKE', 'OTHER', 'HOW', 'THEN', 'ITS', 'OUR', 'TWO', 'MORE', 'THESE', 'WANT',
+                'WAY', 'LOOK', 'FIRST', 'ALSO', 'NEW', 'BECAUSE', 'DAY', 'MORE', 'USE',
+                'NO', 'MAN', 'FIND', 'HERE', 'THING', 'GIVE', 'MANY', 'WELL', 'WAS', 'HAD', 'SAID']
+
 wordList = []
 wordListTotal = []
 wordDictionary = {}
@@ -31,16 +40,17 @@ for line in file:
     for word in words:
         word = word.upper()
         word = re.sub(r'[^\w\s]','', word)
-        wordList.append(word)
-        if word not in wordListTotal:
-            wordListTotal.append(word)
+        if word not in wordsToFilter:
+            wordList.append(word)
+            if word not in wordListTotal:
+                wordListTotal.append(word)
 
 for word in wordListTotal:
     if wordList.count(word) > wordMinimum:
         wordDictionary[word] = wordList.count(word)
     else: continue
 
-sortedDict = sorted(wordDictionary.items(), key = operator.itemgetter(1), reverse= True)
+sortedDict = sorted(wordDictionary.items(), key = operator.itemgetter(1), reverse = True)
 
 for pair in sortedDict:
     print(pair[0] + ": " + str(pair[1]))
